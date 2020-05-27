efbcc2ad43448a644506c1ccfc7fd008995f6674
Sky Qiu <tq21@uw.edu>
2020-05-27 15:33:39 -0700

    added some more plots

--- analysis.R
+++ analysis.R
@@ -27,7 +27,6 @@ toefl_hist <- ggplot(df, aes(x = TOEFL.Score)) +
   theme(plot.title = element_text(hjust = 0.5)) +
   geom_vline(xintercept = toefl_mean, color = "red", size = 1.5)
 
-
 gpa_mean <- mean(df$CGPA)
 gpa_hist <- ggplot(df, aes(x = CGPA)) +
   geom_histogram(color = "black", fill = "skyblue", binwidth = 0.2) +
@@ -35,15 +34,22 @@ gpa_hist <- ggplot(df, aes(x = CGPA)) +
   theme(plot.title = element_text(hjust = 0.5)) +
   geom_vline(xintercept = gpa_mean, color = "red", size = 1.5)
 
+box_1 <- ggplot(df, aes(x = factor(University.Rating), y = GRE.Score, fill = factor(Research))) + 
+  geom_boxplot()
 
+box_2 <- ggplot(df, aes(x = factor(University.Rating), y = TOEFL.Score, fill = factor(Research))) + 
+  geom_boxplot()
 
+box_3 <- ggplot(df, aes(x = factor(University.Rating), y = SOP, fill = factor(Research))) + 
+  geom_boxplot()
 
+box_4 <- ggplot(df, aes(x = factor(University.Rating), y = LOR, fill = factor(Research))) + 
+  geom_boxplot()
 
+box_5 <- ggplot(df, aes(x = factor(University.Rating), y = CGPA, fill = factor(Research))) + 
+  geom_boxplot()
 
-
-
-
-
-
+box_6 <- ggplot(df, aes(x = factor(University.Rating), y = Chance.of.Admit, fill = factor(Research))) + 
+  geom_boxplot()
 
 
